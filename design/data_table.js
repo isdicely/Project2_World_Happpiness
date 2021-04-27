3c64fffce587562df1a09f7f80b993e6f9be1192
isdicely <75281465+isdicely@users.noreply.github.com>
2021-04-26 19:52:02 -0700

    updated links

--- design/data_table.js
+++ design/data_table.js
@@ -5,6 +5,12 @@ d3.csv("/data/TOTAL_2019_2020_clean.csv").then(function (data) {
   const countryDropdownMenuSelection = d3.selectAll("#selCountry");
   const countryDropdownMenu = countryDropdownMenuSelection.node();
   const unique_countries = new Set(data.map((entry) => entry.Country));
+  // Add a opt for all countries
+  const opt_all_countries = document.createElement("option");
+  opt_all_countries.value = "";
+  opt_all_countries.innerHTML = "---";
+  countryDropdownMenu.appendChild(opt_all_countries);
+  // Add opts for all contries to the dropdown menu
   [...unique_countries].sort().forEach((country) => {
     const opt = document.createElement("option");
     opt.value = country;
@@ -12,6 +18,23 @@ d3.csv("/data/TOTAL_2019_2020_clean.csv").then(function (data) {
     countryDropdownMenu.appendChild(opt);
   });
   console.log(countryDropdownMenu);
+
+  // Set Region dropdown menu
+  const regionDropdownMenuSelection = d3.selectAll("#selRegion");
+  const regionDropdownMenu = regionDropdownMenuSelection.node();
+  const unique_regions = new Set(data.map((entry) => entry.Region));
+  // Add opt for no regions
+  const opt_no_region = document.createElement("option");
+  opt_no_region.value = "";
+  opt_no_region.innerHTML = "---";
+  regionDropdownMenu.appendChild(opt_no_region);
+  // Add opts for all regions
+  [...unique_regions].sort().forEach((region) => {
+    const opt_element = document.createElement("option");
+    opt_element.value = region;
+    opt_element.innerHTML = region;
+    regionDropdownMenu.appendChild(opt_element);
+  });
 
   // Append Table heading and Column names
   const table = document.querySelector("table");
@@ -52,16 +75,36 @@ d3.csv("/data/TOTAL_2019_2020_clean.csv").then(function (data) {
     // Prevent the page from refreshing
     event.preventDefault();
     // Clear any input
-    while(tbody.firstChild) tbody.removeChild(tbody.lastChild);
+    while (tbody.firstChild) tbody.removeChild(tbody.lastChild);
+    // Retrive country selected
     const country_selected = countryDropdownMenu.value;
+    // Retrive region selected
+    const region_selected = regionDropdownMenu.value;
+    
+    if (country_selected === "") {
+      if (region_selected === "") {
+        make_table(data);
+      } else {
+        // Filter data based on selected region
+        const region_filtered_display_data = data.filter(
+          (entry) => entry.Region === region_selected
+        );
+        
+        make_table(region_filtered_display_data);
+      }
+    } else {
+      // Filter data based on selected country
+      const country_filtered_display_data = data.filter(
+        (entry) => entry.Country === country_selected
+      );
 
-    // Filter data based on selected country
-    const filtered_display_data = data.filter(
-      (entry) => entry.Country === country_selected
-    );
-    console.log(filtered_display_data);
-    make_table(filtered_display_data);
+      make_table(country_filtered_display_data);
+    }
   }
   // Create event handler, set on chage
   countryDropdownMenuSelection.on("input", updateTable);
+  
+  // move this to the new function***********
+
+  regionDropdownMenuSelection.on("input", updateTable);
 });
